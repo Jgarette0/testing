3c69bb10f99817ca60e813a220077517224af5db
Jgarette0 <jiliangarette@gmail.com>
2023-11-11 02:10:40 +0800

    commit

--- sidebar/sidebar.php
+++ sidebar/sidebar.php
@@ -11,10 +11,46 @@
   <div class="asideContains">
 
   <div class="teacherDetail">
-   <div class="teacherName">Jilian Garette Abangan</div>
-   <div class="teacherInfo" id="one">Computer Laboratory</div>
+   <div class="teacherName">
+   <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="nameSvg"><path d="m9 18 6-6-6-6"/>
+  </svg>
+    <div>Jilian Garette Abangan</div>
+   </div>
+   <div class="teacherInfo" id="">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="classroomSvg lucide-corner-down-right"><polyline points="15 10 20 15 15 20"/><path d="M4 4v7a4 4 0 0 0 4 4h12"/>
+    </svg>
+    <div>Computer Laboratory</div>
+    <div class="class-timer">59:20</div>
+   </div>
    </div>
 
+   <div class="teacherDetail">
+   <div class="teacherName">
+   <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="nameSvg"><path d="m9 18 6-6-6-6"/>
+  </svg>
+    <div>Jonathan Libumfossil</div>
+   </div>
+   <div class="teacherInfo" id="">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="classroomSvg lucide-corner-down-right"><polyline points="15 10 20 15 15 20"/><path d="M4 4v7a4 4 0 0 0 4 4h12"/>
+    </svg>
+    <div>Lecture Room</div>
+    <div class="class-timer">14:12</div>
+   </div>
+   </div>
+
+   <div class="teacherDetail">
+   <div class="teacherName">
+   <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="name"><path d="m9 18 6-6-6-6"/>
+  </svg>
+    <div>Marcus Aurelius</div>
+   </div>
+   <div class="teacherInfo" id="one">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="classroomSvg lucide-corner-down-right"><polyline points="15 10 20 15 15 20"/><path d="M4 4v7a4 4 0 0 0 4 4h12"/>
+    </svg>
+    <div>Lecture Room</div>
+    <div class="class-timer">14:12</div>
+   </div>
+   </div>
 
 
    <div class="teacherDetail">
