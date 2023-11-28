47170a70ccf5729cc1d7b5f878a1a9379f358711
Jgarette0 <aajiliangarette@gmail.com>
2023-11-29 04:04:33 +0800

    final

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
  <link rel="preconnect" href="https://fonts.googleapis.com">
  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
  <link href="https://fonts.googleapis.com/css2?family=Assistant&display=swap" rel="stylesheet">
+ <link rel="stylesheet" href="validation/validation.css">
  <link rel="stylesheet" href="layout/school-layout.css">
  <link rel="stylesheet" href="sidebar/sidebar.css">
  <link rel="stylesheet" href="layout/zoom.css">
@@ -18,8 +19,10 @@
  <?php include 'layout/school-layout.php';?>
  <?php include 'layout/zoom.php';?>
  <?php include 'mock/help-detail.php';?>
+ <?php include 'validation/validation.php';?>
  <script src="sidebar/sidebar.js"></script>
  <script src="layout/zoom.js"></script>
+ <script src="validation/validation.js"></script>
 </body>
 
 </html>
---
+++ validation/validation.js
@@ -0,0 +1,43 @@
+const classroom = document.querySelector(".green");
+const dialog = document.getElementById("dialog");
+const timerDialog = document.getElementById("timer-dialog");
+const close = document.querySelector(".close-button");
+const submits = document.querySelector(".submits");
+const inputField = document.querySelector('input[type="tel"]');
+const form = document.querySelector(".form");
+const submittedSvg = document.querySelector(".submits-svg");
+const closedSvg = document.querySelector(".close-svg");
+
+classroom.addEventListener("click", () => {
+  dialog.showModal();
+});
+close.addEventListener("click", () => {
+  dialog.close();
+});
+
+submits.addEventListener("click", () => {
+  setTimeout(() => {
+    timerDialog.showModal();
+  }, 500);
+});
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  document.querySelector('input[type="tel"]').value = "";
+});
+
+submits.disabled = true;
+
+inputField.addEventListener("input", function (event) {
+  submits.disabled = !event.target.value.trim();
+});
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  inputField.value = "";
+  submits.disabled = true;
+});
+submittedSvg.addEventListener("click", () => {
+  submittedSvg.classList.toggle("submitted-svg");
+  closedSvg.classList.toggle("closed-svg");
+});
